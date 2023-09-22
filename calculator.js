70a6d164b3f906e753b3c94c1ab7e76f6ad2ced4
kjartano47 <kjartano97@gmail.com>
2023-09-22 22:13:15 +0000

    Add display functionality

--- calculator.js
+++ calculator.js
@@ -31,9 +31,30 @@ let multiply = (num1, num2) => {
 let divide = (num1, num2) => {
 	return num1 / num2;
 };
-console.log(operate(5, "+", 5));
-console.log(operate(5, "-", 5));
-console.log(operate(5, "*", 5));
-console.log(operate(5, "/", 5));
 
 //build your own function to evaluate expressions as part of this calculator project
+let test = 0;
+const container = document.querySelector("#container");
+let arr = [];
+let inputTest = (input) => {
+	arr.push(input);
+	for (let i = 0; i < arr.length; i++) {
+		arr.join("^");
+		content.textContent = `${arr.join("")}`;
+	}
+	console.log(arr);
+};
+
+let displayValue = [];
+console.log(test);
+const seven = document.querySelector(".seven");
+seven.addEventListener("click", (e) => {
+	console.log("Seven was pressed!");
+	displayValue.push(7);
+	console.log(displayValue);
+});
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = 0;
+
+container.appendChild(content);
